8a2b34fb422a8984c5b60c96dd10542f2b95d206
annap-ship-it <248465354+annap-ship-it@users.noreply.github.com>
2026-03-18 20:17:15 +0100

    Added GA tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,32 @@ import { CookiesConsent } from "@/components/cookies-consent"
 import { ScrollToTop } from "@/components/scroll-to-top"
 import { ClientOnlyPopups } from "@/components/client-only-popups"
 import "./globals.css"
+import Script from "next/script";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="uk">
+      <head />
+      <body>
+        {children}
+
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-CNTQ2P0HEG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CNTQ2P0HEG');
+          `}
+        </Script>
+      </body>
+    </html>
+  );
+}
 
 const onest = Onest({
   subsets: ["latin"],
